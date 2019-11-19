25b603ac5f4c51ec21e8fcb608b6125812384ec3
Chris Anderson <chris.anderson955@gmail.com>
2019-11-18 20:52:43 -0700

    End of class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,24 @@ class PagesController < ApplicationController
   end
 
   def new
+  	@page = Page.new
+  end
+
+  def create
+  	@page = Page.new(pages_params)
+
+  	if @page.save 
+  		redirect_to pages_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
   end
+
+  private
+  	def pages_params
+  		params.require(:page).permit(:title, :author, :body)
+  	end
 end
